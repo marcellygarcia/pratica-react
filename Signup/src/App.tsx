59a9ee0b1eeef005a2b338c0db0c8d67c404cd7f
Marcelly Garcia <mgmm@icomp.ufam.edu.br>
2023-04-07 12:10:43 -0400

    Inclusão da Imagem principal

--- Signup/src/App.tsx
+++ Signup/src/App.tsx
@@ -1,17 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+//import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ReactLogo from './assets/Logo.svg';
+
 
 export function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div id='div-2'>
-
+    <div className='container'>
+      <img src={ReactLogo}/>
+      <div className='div-1'>
+      
       </div>
+      
+ 
     </div>
+    
   )
 }
 
